221c0845c4e6802ca249b8303cdd4cfe01775f5f
rahmanz11 <zillur.babu@gmail.com>
2022-09-17 21:33:46 +0600

    modified

--- bigq.py
+++ bigq.py
@@ -40,30 +40,50 @@ def create_table(client, dataset_id) -> string:
     # print("Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id))
     return table_id
 
-def insert_data(table_id, client):
-    
+
+def fetch_data():
+    data = None
+    # Fetch data
     url = "https://api.plos.org/search?q=title:%22Drosophila%22%20AND%20body:%22RNA%22&fl=id,abstract&wt=json"
-    response = requests.get(url, data=None,headers={"Content-Type": "application/json"})
-    data = [response.json()['response']]
-    errors = client.insert_rows_json(table_id, data)
-    if errors == []:
-        print("New rows have been added.")
+    try:
+        response = requests.get(url, data=None, headers={"Content-Type": "application/json"})
+        if response:
+            data = [response.json()['response']]
+    except Exception as e:
+        print("Exception occurred while fetching data {}".format(str(e))) 
+    
+    return data        
+
+def insert_data(table_id, client):
+    data = fetch_data()
+    if data is not None:
+        errors = client.insert_rows_json(table_id, data)
+        if errors == []:
+            print("New records have been added.")
+        else:
+            print("Encountered errors while inserting rows: {}".format(errors))
     else:
-        print("Encountered errors while inserting rows: {}".format(errors))
+        print("No data available")
 
 
 if __name__=="__main__":
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="sanguine-tome-362813-96fc8db73f4f.json"
     client = bigquery.Client()
 
+    # Initially when no dataset and table are avilable, 
+    # Create dataset to get the dataset id and 
+    # then create table to get the table id
+
     # Create dataset
     # dataset_id = create_dataset(client)
     # print("Dataset id {}".format(dataset_id))
 
-    dataset_id = 'sanguine-tome-362813.mydataset'
+    dataset_id = 'sanguine-tome-362813.mydataset' # dataset_id got from create_dataset
     # Create table
-    # table_id = create_table(client, dataset_id)
+    table_id = create_table(client, dataset_id)
     # print("Table id {}".format(table_id))
 
-    table_id = "sanguine-tome-362813.mydataset.mytable"
+    # table_id = "sanguine-tome-362813.mydataset.mytable" # table_id got from create_table
+
+    # Now insert data
     insert_data(table_id, client)
